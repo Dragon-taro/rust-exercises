4a4e58e4c724d56458cf188dff9da171a2adffd6
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-08-20 00:39:48 +0900

    ownership

--- exercises/ownership/src/main.rs
+++ exercises/ownership/src/main.rs
@@ -1,24 +1,26 @@
 #![allow(dead_code)]
 fn main() {
     let (word_a, word_b) = words();
-    let message = format!("{}{}！", word_a, word_b);
+    let message = &format!("{}{}！", word_a, word_b);
     output(message);
     // ゴール4：次の行をアンコメントしても実行できるようにしてください
-    //output(message);
+    output(message);
 }
 
 fn words() -> (String, String) {
     (format!("こんにちは"), format!("世界"))
 }
 
-fn output(text: String) {
+fn output(text: &String) {
+    // ゴール1
+    // let kanji_only = remove_hiragana(text.clone());
     let kanji_only = remove_hiragana(text);
     println!("{}", kanji_only);
     /*
     ゴール2：次の行をアンコメントすると何がおきるでしょうか？
     これをコンパイルを通すにはどうすれば良いでしょうか？
     */
-    // println!("ひらがなを抜き取ると：{:?} → {:?}", text, kanji_only);
+    println!("ひらがなを抜き取ると：{:?} → {:?}", text, kanji_only);
 
     /*
     ゴール3：データをコピーせずにコンパイルを通すにはどおすれば良いでしょうか？
@@ -26,11 +28,11 @@ fn output(text: String) {
     */
 }
 
-fn remove_hiragana(text: String) -> String {
+fn remove_hiragana(text: &String) -> String {
     /*
      ゴール1：コンパイルを通すには何を変更すれば良いでしょうか
     */
-    let result = String::new();
+    let mut result = String::new();
     for c in text.chars() {
         if c < 'ぁ' || 'ん' < c {
             result.push(c);
